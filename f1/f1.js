adc81ea27bac1f84dd86ca1c8a89ee1aca12f664
Nokiloki700 <lenard700@gmail.com>
2024-10-08 13:24:08 +0200

    sajt

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,12 @@
-
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
-async function f1 (lat, long, timeStr) {
-    //TODO
+const APIAdress = 'https://archive-api.open-meteo.com/v1/archive';
+async function f1(lat, long, timeStr) {
+    let sum = 0;
+    let asd;
+    await fetch(APIAdress + `?latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`)
+        .then(data => data.json()).then(data => asd = data).then(data => console.log(data));
+    asd.hourly.temperature_2m.forEach(element => {
+        sum += element;
+    });
+    return Math.floor(sum / 24);
 }
-
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -1,6 +1,9 @@
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const APIAdress = 'https://archive-api.open-meteo.com/v1/archive';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    //TODO
+    let asd;
+    await fetch(APIAdress + `?latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`)
+        .then(data => data.json()).then(data => asd = data).then(data => console.log(data));
+    return asd.hourly.temperature_2m.at(hour)
 }
 module.exports = f2;
